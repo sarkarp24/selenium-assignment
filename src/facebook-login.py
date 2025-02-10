4e23f8520013701d6bd59254a47498f9f0153973
sarkarp24 <sarkar.prodyot@gmail.com>
2025-02-09 21:55:24 -0800

    first commit

--- src/facebook-login.py
+++ src/facebook-login.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.by import By
 from selenium.webdriver.chrome.service import Service
 import time
 import os
