556a8153140d115859cc5b678c627e5040249feb
sarkarp24 <sarkar.prodyot@gmail.com>
2025-02-09 22:35:29 -0800

    first commit

--- src/facebook-login.py
+++ src/facebook-login.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import shutil
+import tempfile 
 import time
 import os
+
+
+options = Options()
+#options.add_argument('--headless')
+#options.add_argument('--no-sandbox')
+#options.add_argument('--disable-dev-shm-usage')
+
+# Create a temporary directory for the user data
+user_data_dir = tempfile.mkdtemp()
+options.add_argument(f"--user-data-dir={user_data_dir}")  # Use f-string
 
 service_option = Service("/usr/bin/chromedriver")
 driver = webdriver.Chrome(service=service_option)
@@ -19,3 +32,4 @@ username = driver.find_element(By.XPATH,"//input[@name='email']").send_keys(user
 #driver.find_element(By.XPATH,"//input[@name='pass']").send_keys(pwd)
 #driver.find_element(By.CSS_SELECTOR,"button[name='login']").click()
 time.sleep(2)
+shutil.rmtree(user_data_dir)
